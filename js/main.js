dbbfe5e13b477f1427e10d8de8f35b21c03d8dd1
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-20 13:12:23 +0500

    JS 4-dars

--- js/main.js
+++ js/main.js
@@ -1,51 +1,21 @@
-/* HTML bilan ishlash */
+let belgilar = 'qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
 
-/*let headTag = document.head;
-let bodyTag = document.body;
-let havola = document.documentURI;
-let formula = document.forms;
-console.log(formula)*/
-
-/* Events in JS (onclick, ondblclick, onchange, onfocus, onblur, oninput) */
-/*function battonniBos(){
-    alert("Siz battonni bir marta bostingiz!");
-}*/
-
-/*function battonniIkkiMartaBos(){
-    alert("Siz battonni ikki marta bostingiz!");
-}*/
-
-/*function tanlash(event){
-    alert("Siz " + event.target.value + "ni tanladingiz!");
-}*/
-
-/*function inputgaBosish(){
-    console.log("Inputga bosildi!");
-}*/
-
-/*function inputdanChiqish(){
-    console.log("Inputdan chiqildi!");
-}*/
-
-/*function yozish(event){
-    console.log(event.target.value);
-}*/
-
-/*
-document.querySelector('.classNomi').addEventListener('copy', function (event){
-    console.log("Diqqat! Sizdan '" + event.target.value + "' nomli fayllar ko'chirilyapti")
-});*/
-
-function harakatlanish(e){
-    console.log(e.clientX,e.clientY);
+function parolBer(){
+    let i = 0;
+    let parol = '';
+    while (i < 8){
+        let son = parseInt(Math.random()*belgilar.length);
+        parol += belgilar[son];
+        i++;
+    }
+    document.querySelector('input').value = parol;
 }
-
-function kirish(){
-    console.log("Hududga kirildi!");
-}
-
-function chiqish(){
-    console.log("Hududdan chiqildi!");
-}
-
+document.querySelector('.tugma').addEventListener('click',function (){
+    let ekran = document.querySelector('.ekran');
+    console.log(typeof parseInt(ekran.value));
+    ekran.value = parseInt(ekran.value)+1;
+});
+document.querySelector('.nolga').addEventListener('click',function (){
+   document.querySelector('.ekran').value = 0;
+});
 
