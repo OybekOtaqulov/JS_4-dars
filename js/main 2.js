5a065602a1019ae7f32df7d4b97a6037d8fa7143
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-20 12:40:28 +0500

    JS 4-dars

---
+++ js/main 2.js
@@ -0,0 +1,12 @@
+/*document.querySelector('.classNomi').addEventListener('copy', function (event){
+    alert("Diqqat! Agarda '" + event.target.value + "'ni tanlasangiz, hech narsa bo'lmaydi")
+});*/
+
+function tanlash(event){
+    alert("Diqqat! Agarda '" + event.target.value + "'ni tanlasangiz, hech narsa bo'lmaydi");
+}
+
+
+
+
+
